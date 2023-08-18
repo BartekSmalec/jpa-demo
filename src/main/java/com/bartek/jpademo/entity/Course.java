a7d754364ebfeaa14ad3d1c458db86a3b25c4ff0
bartek <smalecbartlomiej@gmail.com>
2023-08-18 21:38:49 +0200

    Added UpdateTimestamp and CreationTimestamp annotations

--- src/main/java/com/bartek/jpademo/entity/Course.java
+++ src/main/java/com/bartek/jpademo/entity/Course.java
@@ -2,6 +2,10 @@ package com.bartek.jpademo.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -9,11 +13,19 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Table(name = "CourseDetails")
 public class Course {
     @Id
     @GeneratedValue
     private Long id;
+
+    @Column(name = "fullname", nullable = false)
     private String name;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdatedDate;
+    @CreationTimestamp
+    private LocalDateTime createdDate;
 
     public Course(String name) {
         this.name = name;
